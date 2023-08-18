fd6c8ce6f8a2094fffbbc51fcb9342d439b4d8bc
Rashad Ramaileh <rashadramaileh@gmail.com>
2023-08-18 16:11:24 -0600

    fixed static directory i think

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = 'pokerMain:index'
 LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
